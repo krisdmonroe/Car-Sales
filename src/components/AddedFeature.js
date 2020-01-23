da9318796fc9c7082fcfd8ff84ae9b3b1b00cfc8
Kris Monroe <krisdmonroe@gmail.com>
2020-01-22 20:06:41 -0800

    mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux' // HOC
-
+import { removeFeature } from '../actions/removeFeatureAction'
 const AddedFeature = props => {
+  console.log('this is second props',props )
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
+const mapStateToProps = (state) => {
+  return {
+     key:state.additionalFeatures.id,
+  }
+}
 
-//wrap component in the second connect function call
-export default connect(
- (state) => {
-   return {
-      titleOnProps:state.title
-   }
-},
-  {}
-  )(AddedFeature); // function currying
+export default connect(mapStateToProps,{removeFeature})(AddedFeature)
--- src/reducers/carReducer.js
+++ src/reducers/carReducer.js
@@ -17,7 +17,26 @@ export const initialState = {
  
  export const carReducer = (state = initialState, action) => {
     switch(action.type){
+        case `ADD-FEATURE`:
+            return {...state,
+            additionalPrice: state.additionalPrice + action.payload.price,
+                car: {
+                    ...state.car, features:[
+                        ...state.car.features,
+                        action.payload
+                    ]
+                }
+            }
+            case `REMOVE-FEATURE`:
+                return {...state,
+                    additionalPrice: state.additionalPrice - action.payload.price,
+                    car: {
+                        ...state.car, features: [...state.car.features.filter(feature => 
+                             feature.id !== action.payload.id)
+                        ]
+                    } 
+                }    
         default:
-            return state
+            return state;
     }
 }
